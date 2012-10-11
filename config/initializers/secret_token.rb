1ba65e866fa39af275fe78360befa1971581c7ba
faisal <faisal.akhlaq@devbatch.com>
2012-10-11 11:57:58 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevBatchAppStore::Application.config.secret_token = '4258e79ffc3e50b15be0d26d7724ac044e64ae6e8568c801c30c4dbe8843fd489dfd3a8d995b26cbea111c18a556b752520e74cc1db16aac0f4c28f80b964ca7'
